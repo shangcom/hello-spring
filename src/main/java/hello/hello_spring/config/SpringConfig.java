619f3bac111e1ea8e68faac4bfea8279a04c0ef5
shangcom <dndbs91@gmail.com>
2024-06-24 21:51:00 +0900

    SpringConfig에서 Repository 교체중.

--- src/main/java/hello/hello_spring/config/SpringConfig.java
+++ src/main/java/hello/hello_spring/config/SpringConfig.java
@@ -1,13 +1,27 @@
 package hello.hello_spring.config;
 
+import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    /**
+     * 아래 JdbcMemberRepository 생성자에 매개변수 dataSource 필요함.
+     */
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +30,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
